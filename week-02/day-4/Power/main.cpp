684828ba145d7e56caca725e22bf78b1913c71d0
Miklos Bognar <nicobognar@gmail.com>
2019-04-11 16:42:58 +0200

    Corrected Power task

--- week-02/day-4/Power/main.cpp
+++ week-02/day-4/Power/main.cpp
@@ -3,12 +3,13 @@
 int oldSpice(int thisto, int thepowerof);
 
 int main() {
-    std::cout << oldSpice(4,2) << std::endl;
+    std::cout << oldSpice(3,2) << std::endl;
     return 0;
 }
 int oldSpice(int thisto, int thepowerof){
-    thepowerof + 2;
-    if (thisto >= 1 && thepowerof >= 1){
-        return ((thisto * thisto)* (thepowerof - 1));
+    if (thepowerof == 1){
+        return thisto;
+    } else{
+        return thisto * oldSpice(thisto, thepowerof - 1);
     }
 }
